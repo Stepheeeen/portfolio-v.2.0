291a1e63e041e28b97deaf8ba657d19f407b1b90
stepheeeen <stephenon56@gmail.com>
2024-07-29 16:09:38 +0100

    completed _home

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,8 +3,9 @@ import { Link, useLocation } from 'react-router-dom';
 import Twitter from '../assets/icon/TwitterIcon.svg'
 import Github from '../assets/icon/GithubIcon.svg'
 import Instagram from '../assets/icon/InstagramIcon.svg'
+import Hamburger from '../assets/icon/HamburgerIcon.svg'
 
-const Dashboard = ({ children }) => {
+const Dashboard = ({ children, bgImage }) => {
     const location = useLocation();
     const pathname = location.pathname;
 
@@ -17,16 +18,20 @@ const Dashboard = ({ children }) => {
     const footerLinks = [
         { name: <img src={Twitter} className=''/>, path: '#' },
         { name: <img src={Instagram} className=''/>, path: '#' },
+        { name: <img src={Github} className=''/>, path: 'https://github.com/stepheeeen', visible: 'md:hidden' },
     ];
 
     return (
-        <div className='relative bg-[#011627] w-[98%] m-auto h-[95vh] rounded border border-1 border-[#1E2D3D] overflow-y-scroll'>
+        <div className={`relative bg-[#011627] w-[95.6%] md:w-[98%] m-auto h-[95vh] rounded border-2 border-[#1E2D3D] overflow-hidden`}>
             <nav className='absolute w-full h-[45px] border border-1 border-[#1E2D3D] top-0 left-0 z-10 bg-inherit flex items-center justify-between'>
                 <div className='h-full flex items-center font-[500] text-[14px]'>
-                    <div className='border border-1 border-[#1E2D3D] p-[11px] w-[270px] text-[#607B96]'>
+                    <div className='flex items-center justify-between md:border md:border-1 md:border-[#1E2D3D] p-[11px] w-[97vw] md:w-[250px] text-[#607B96]'>
                         <Link to='/'>Stephen-Onucheyo</Link>
+                    <button className=' mr-2 md:hidden'>
+                    <img src={Hamburger} className='mr-1'/>
+                    </button>
                     </div>
-                    <ul className='flex'>
+                    <ul className='hidden md:flex'>
                         {links.map((link, i) => (
                             <li key={i}>
                                 <Link to={link.path} className={`border border-1 border-[#1E2D3D] p-[12px] w-[130px] hover:text-[#d7d7d7c2] ${pathname === link.path ? 'text-white  active' : 'text-[#607B96]'}`}>
@@ -35,25 +40,26 @@ const Dashboard = ({ children }) => {
                             </li>
                         ))}
                     </ul>
+
                 </div>
                 <div>
-                    <Link to='/contact' className={`border border-1 border-[#1E2D3D] p-[11px] w-[130px] hover:text-[#d7d7d7c2] ${pathname === '/contact' ? 'text-white' : 'text-[#607B96]'}`}>
+                    <Link to='/contact' className={`hidden md:block border border-1 border-[#1E2D3D] p-[10px] w-[130px] hover:text-[#d7d7d7c2] ${pathname === '/contact' ? 'text-white' : 'text-[#607B96]'}`}>
                         _contact-me
                     </Link>
                 </div>
             </nav>
-            <section className='mt-[50px] p-3'>
+            <section className={`pt-[46px] pb-[33px] p-3 ${bgImage} h-[]`}>
                 {children}
             </section>
             <footer className='absolute w-full h-[33px] border border-1 border-[#1E2D3D] bottom-0 left-0 z-10 bg-inherit flex items-center justify-between'>
                 <div className='h-full flex items-center font-[500] text-[14px]'>
-                    <div className='border border-1 border-[#1E2D3D] flex items-center px-3 h-full w-[130px] text-[#607B96]'>
+                    <div className='border border-1 border-[#1E2D3D] flex items-center px-3 h-full w-[270px] md:w-[130px] text-[#607B96]'>
                         find me in:
                     </div>
                     <ul className='flex items-center'>
                         {footerLinks.map((link, i) => (
                             <li key={i} className='flex items-center'>
-                                <Link to={link.path} className={`border border-1 px-2 py-[3px] border-[#1E2D3D] ${pathname === link.path ? 'text-white' : 'text-[#607B96]'}`}>
+                                <Link to={link.path} className={`border border-1 px-2 py-[3px] border-[#1E2D3D] ${link.visible} ${pathname === link.path ? 'text-white' : 'text-[#607B96]'}`}>
                                     {link.name}
                                 </Link>
                             </li>
@@ -61,7 +67,7 @@ const Dashboard = ({ children }) => {
                     </ul>
                 </div>
                 <div>
-                <Link to='https://github.com/stepheeeen' className={`flex items-center justify-center border border-1 border-[#1E2D3D] p-[4px] w-[130px] hover:text-[#d7d7d7c2] text-[#607B96]`}>
+                <Link to='https://github.com/stepheeeen' className={`hidden md:flex items-center justify-center border border-1 border-[#1E2D3D] p-[4px] w-[130px] hover:text-[#d7d7d7c2] text-[#607B96]`}>
                         @Steph4n <span><img src={Github} className=' ml-[7px]'/></span>
                     </Link>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import Dashboard from '../components/Dashboard';
+import Image from '../assets/icon/SampleImage.png'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <Dashboard>
-            <div className="">
-                {/* <h1 className="text-4xl font-bold">About Page</h1> */}
+        <Dashboard bgImage={'bgImage'}>
+            <div className="w-full h-[90vh] flex items-center justify-center">
+                <div className='flex items-center md:ml-[80px]'>
+                    <div className='p-[15px] md:mr-[25px] md:w-auto md:mt-[-25px]'>
+                        <p>Hi all. i am</p>
+
+                        <h1 className='text-6xl my-2 md:my-1'>Stephen <br className='hidden md:block' /> Onucheyo</h1>
+                        <p className='text-2xl md:text-xl md:text-[#4D5BCE] text-[#43D9AD]'> <span>&gt;</span> Software Developer</p>
+
+                        <div className='mt-[170px] md:mt-[50px]'>
+                        <p className='md:block text-[#607B96] text-[15px] hidden'>// you can also see my Github page</p>
+                        <p className='text-[#607B96] text-[15px] md:hidden'>// find my profile on Github:</p>
+
+                        <p className='text-[15px]'>
+                            <span className='text-[#4D5BCE]'>const</span>
+                            <span className='mx-2 text-[#43D9AD]'>githubLink</span>
+                            =
+                            <Link to={'https://github.com/stepheeeen'} className='ml-2 text-[#E99287]'>"https://<br className='md:hidden'/>github.com/stepheeeen"</Link>
+                        </p>
+                        </div>
+                    </div>
+                    <div className='hidden md:block'>
+                        <img src={Image} alt="" className='w-[350px]' />
+                    </div>
+                </div>
             </div>
         </Dashboard>
     );
